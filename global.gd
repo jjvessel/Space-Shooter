52751f532fd7fa960145eb54870136581235beaa
jjvessel <143030943+jjvessel@users.noreply.github.com>
2023-09-24 13:17:59 -0400

    Complete

--- global.gd
+++ global.gd
@@ -1,9 +1,66 @@
 extends Node
 
+var VP = Vector2.ZERO
+
+var score = 0
+var time = 0
+var lives = 0
+
+
 func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	randomize()
+	VP = get_viewport().size
+	var _signal = get_tree().get_root().size_changed.connect(_resize)
+	reset()
 
 func _process(_delta):
-	if Input.is_action_pressed("Quit"):
-		get_tree().quit()
-		
+	if Input.is_action_just_pressed("Quit"):
+		var Menu = get_node_or_null("/root/Game/UI/Menu")
+		if Menu == null:
+			get_tree().quit()
+		else:
+			if Menu.visible:
+				get_tree().paused = false
+				Menu.hide()
+			else:
+				get_tree().paused = true
+				Menu.show()
+	var Asteroid_Container = get_node_or_null("/root/Game/Asteroid_Container")
+	var Enemy_Container = get_node_or_null("/root/game/Enemy_Container")
+	if Asteroid_Container != null and Enemy_Container != null:
+		if Asteroid_Container.get_child_count() == 0 and Enemy_Container.get_child_count() == 0:
+			get_tree().change_scene_to_file("res://UI/End_Game.tscn")
+
+
+func update_score(s):
+	score += s
+	var hud = get_node_or_null("/root/Game/UI/HUD")
+	if hud != null:
+		hud.update_score()
+
+func update_lives(l):
+	lives += l
+	var hud = get_node_or_null("/root/Game/UI/HUD")
+	if hud != null:
+		hud.update_lives()
+	if lives < 0:
+		get_tree().change_scene_to_file("res://UI/end_game.tscn")
+
+func update_time(t):
+	time += t
+
+
+func _resize():
+	VP = get_viewport().size
+	var hud = get_node_or_null("/root/Game/UI/HUD")
+	if hud != null:
+		hud.update_lives()
+
+
+
+func reset():
+	get_tree().paused = false
+	score = 0
+	time = 30
+	lives = 5
